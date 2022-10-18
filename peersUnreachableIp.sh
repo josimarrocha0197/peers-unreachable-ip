661c518b2b08fa3f2af78e1a9ed429ab1f78c1b0
Josimar <josimar@futurotec.com.br>
2022-10-18 12:00:26 -0300

    Primeiro commit

---
+++ peersUnreachableIp.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+# Script: imprime o IP de cada peer que ficou com o status de "unreachable"
+
+logDir="/var/log/asterisk/messages"
+
+for ramal in $(cat ${logDir} | grep -i unreach | awk -F "'" '{ print $2 }')
+do
+        echo Ramal ${ramal}
+        asterisk -rx 'sip show peers' | grep ${ramal}
+        echo -e "\n"
+done
